2c6b06a1ce1c7b2c50e4b6423df238815892c1dd
prgrobots <91106994+prgrobots@users.noreply.github.com>
2023-08-17 09:30:26 +0800

    added the do something function

--- dumb.py
+++ dumb.py
@@ -4,3 +4,14 @@
 
 # sensor_temp = DH11
 
+# this won't work
+print('this) 
+
+
+def something_that_does_work():
+    '''This need to take out the rubbish
+    prams : rubbish bool
+    returns : bool
+    
+    '''
+    pass
